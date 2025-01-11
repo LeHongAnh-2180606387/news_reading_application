a30872fe017c87ad590810817cc5a290f793b421
baolaptrinh <109140427+baolaptrinh@users.noreply.github.com>
2025-01-11 14:38:56 +0700

    222

--- lib/config.dart
+++
@@ -1,4 +0,0 @@
-const kBaseUrl = 'https://newsapi.org';
-const kApiUrl = '$kBaseUrl/api';
-const kBasicAuth = 'bff42077feb94c4eaeafbc0891768e33'; // Your API key
-//const kApiKey = 'bff42077feb94c4eaeafbc0891768e33';
